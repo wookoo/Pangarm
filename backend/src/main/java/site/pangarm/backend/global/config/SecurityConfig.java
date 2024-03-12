8b6d10cd164a669c898865229014b8ee5138d594
jiwon83 <gkswldnj0803@gmail.com>
2024-03-12 16:39:42 +0900

    Feat: BE_스프링 시큐리티 Config
    S10P22A509-135

---
+++ backend/src/main/java/site/pangarm/backend/global/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package site.pangarm.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests((authorizeRequests) -> {
+                            authorizeRequests
+                                    .requestMatchers("/api/user/login").permitAll()
+                                    .anyRequest().authenticated();
+                        });
+
+
+        return http.build();
+    }
+
+}
