62892c94412240135555c9bc791189f4dad4a8d2
jiwon83 <gkswldnj0803@gmail.com>
2024-03-14 13:15:12 +0900

    Style: BE_변수명 변경
    S10P22A509-97

--- backend/src/main/java/site/pangarm/backend/application/member/controller/MemberController.java
+++ backend/src/main/java/site/pangarm/backend/application/member/controller/MemberController.java
@@ -1,10 +1,9 @@
 package site.pangarm.backend.application.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import site.pangarm.backend.domain.member.dto.MemberJoinDto;
-import site.pangarm.backend.domain.member.service.MemberService;
+import site.pangarm.backend.application.member.dto.request.MemberSignUpRequest;
+import site.pangarm.backend.domain.member.MemberService;
 
 @RestController
 @RequestMapping("/member")
@@ -13,7 +12,7 @@ public class MemberController {
 
     private final MemberService memberService;
     @PostMapping("/sign-up")
-    public String signUp(@RequestBody MemberJoinDto memberJoinDto) {
+    public String signUp(@RequestBody MemberSignUpRequest memberJoinDto) {
         memberService.signup(memberJoinDto);
         return "회원가입 성공";
     }
--- backend/src/main/java/site/pangarm/backend/domain/member/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package site.pangarm.backend.domain.member.service;
-
-import site.pangarm.backend.domain.member.dto.MemberJoinDto;
-
-public interface MemberService {
-    void signup(MemberJoinDto memberJoinDto);
-}
