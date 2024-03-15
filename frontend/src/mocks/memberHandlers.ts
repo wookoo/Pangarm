142069bd86ce0e72a73f48df9ea2b6d427ee2625
gwanu-dev <devkwanwoo@gmail.com>
2024-03-15 17:53:02 +0900

    Feat: FE_memberHandlers.ts login 함수 추가
    S10P22A509-130

--- frontend/src/mocks/memberHandlers.ts
+++ frontend/src/mocks/memberHandlers.ts
@@ -9,6 +9,22 @@ type SignUpRequestBody = {
   job: string;
 };
 
+type TokenData = {
+  accessToken?: string;
+  refreshToken?: string;
+};
+
+interface LoginRequestBody {
+  email: string;
+  password: string;
+}
+
+interface LoginResponseBody extends LoginRequestBody {
+  data?: TokenData;
+  code?: string;
+  message: string;
+}
+
 const regexEmail = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
 export const memberHandlers = [
@@ -29,9 +45,20 @@ export const memberHandlers = [
       message = ERR_MSG.M001;
     }
 
+    if (status !== 200) {
+      return HttpResponse.json(
+        {
+          code,
+          message,
+        },
+        {
+          status,
+        }
+      );
+    }
+
     return HttpResponse.json(
       {
-        code,
         message,
       },
       {
@@ -40,7 +67,43 @@ export const memberHandlers = [
     );
   }),
 
-  http.post("member/login", () => {}),
+  // 로그인
+  http.post<never, LoginRequestBody>("member/login", async ({ request }) => {
+    const reqData = await request.json();
+    const { email, password } = reqData;
+
+    let code = "";
+    let message = "";
+    if (!regexEmail.test(email) || password.length === 0) {
+      code = "G002";
+      message = ERR_MSG.G002;
+    }
+
+    if (code.length === 0) {
+      return HttpResponse.json(
+        {
+          code,
+          message,
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    return HttpResponse.json(
+      {
+        data: {
+          accessToken: "SomeAccessToken",
+          refreshToken: "SomeRefreshToken",
+        },
+        message: "로그인이 정상적으로 실행됐습니다.",
+      },
+      {
+        status: 200,
+      }
+    );
+  }),
 
   http.post("member/reissue", () => {}),
   http.post("member/precedent", () => {}),
