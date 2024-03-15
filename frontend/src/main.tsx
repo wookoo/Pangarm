53957974ff4e42fac9566e525295ee0011953c12
gwanu-dev <devkwanwoo@gmail.com>
2024-03-15 16:29:44 +0900

    Feat: FE_memberHandlers.ts sign-up 함수 추가
    S10P22A509-130

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
-
 import { Theme } from "@radix-ui/themes";
 
 const enableMocking = async () => {
--- frontend/src/mocks/memberHandlers.ts
+++ frontend/src/mocks/memberHandlers.ts
@@ -1,7 +1,49 @@
 import { http, HttpResponse } from "msw";
+import ERR_MSG from "./ErrorMessage";
+
+type SignUpRequestBody = {
+  email: string;
+  password: string;
+  name: string;
+  gender: number;
+  job: string;
+};
+
+const regexEmail = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
 export const memberHandlers = [
-  http.get("/member/login", () => {
-    return HttpResponse.json();
+  // 회원가입
+  http.post("/member/sign-up", async ({ request }) => {
+    const data = (await request.json()) as SignUpRequestBody;
+    const { email, password, name, job } = data;
+    console.log(`[MSW-memberSignIn]`);
+    console.log(data);
+
+    let status = 200;
+    let message = "회원가입에 성공했습니다.";
+    let code = "SUCCESS";
+
+    if (!regexEmail.test(email) || password.length === 0 || name.length === 0 || job.length === 0) {
+      code = "M001";
+      status = 409;
+      message = ERR_MSG.M001;
+    }
+
+    return HttpResponse.json(
+      {
+        code,
+        message,
+      },
+      {
+        status,
+      }
+    );
   }),
+
+  http.post("member/login", () => {}),
+
+  http.post("member/reissue", () => {}),
+  http.post("member/precedent", () => {}),
+  http.get("member/precedent", () => {}),
+  http.post("member/keyword-subscribe", () => {}),
 ];
