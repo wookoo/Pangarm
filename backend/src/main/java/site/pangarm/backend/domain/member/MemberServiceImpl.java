d5f110b3637bae3b4b834b10ca846be262212400
jiwon83 <gkswldnj0803@gmail.com>
2024-03-18 10:41:18 +0900

    Feat: BE_시큐리티: 회원 가입 시 역할 컬럼 저장로직 추가
    S10P22A509-120

--- backend/src/main/java/site/pangarm/backend/domain/member/MemberServiceImpl.java
+++ backend/src/main/java/site/pangarm/backend/domain/member/MemberServiceImpl.java
@@ -1,11 +1,17 @@
 package site.pangarm.backend.domain.member;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.pangarm.backend.application.member.dto.request.MemberLoginRequest;
 import site.pangarm.backend.application.member.dto.request.MemberSignUpRequest;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -13,6 +19,7 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @Transactional
     @Override
@@ -22,7 +29,9 @@ public class MemberServiceImpl implements MemberService{
             throw new IllegalArgumentException("이미 존재하는 회원입니다.");
         }
         memberDto.setEncodedPassword(bCryptPasswordEncoder.encode(memberDto.getPassword()));
-        memberRepository.save(memberDto.toMemberEntity());
+        Member joinMember = memberDto.toMemberEntity();
+        joinMember.setRole("ROLE_USER");
+        memberRepository.save(joinMember);
     }
 
     @Override
