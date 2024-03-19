4901dac667a40a8b325d5cb315e79e546fcc07fc
김민준 <alswns1285@gmail.com>
2024-03-19 14:02:08 +0900

    Feat: FE_회원가입 폼 Footer 구현

---
+++ frontend/src/components/SignUp/SignUpFormFooter.tsx
@@ -0,0 +1,12 @@
+export default function SignUpFormFooter() {
+  return (
+    <div className="mt-20">
+      <button className="flex w-full items-center justify-center rounded-xl bg-navy p-2 text-2xl text-white">
+        회원가입
+      </button>
+      <div className="mt-2 w-full text-end text-lightgray">
+        이미 회원이신가요?
+      </div>
+    </div>
+  );
+}
