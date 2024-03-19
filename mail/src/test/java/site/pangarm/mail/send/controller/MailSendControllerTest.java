536c69d55d13c4956880b97e1b116bee95ec1ae6
김도현 <nanayagoon@daum.net>
2024-03-19 12:26:25 +0900

    Test: BE_메일 발송 테스트
    S10P22A509-117

--- mail/src/test/java/site/pangarm/mail/send/controller/MailSendControllerTest.java
+++ mail/src/test/java/site/pangarm/mail/send/controller/MailSendControllerTest.java
@@ -4,63 +4,80 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import site.pangarm.mail.send.service.MailSendService;
+import site.pangarm.mail.send.dto.response.MailSendResponse;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest(MailSendController.class)
 class MailSendControllerTest {
 
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
 
     @Autowired
-    ObjectMapper mapper;
+    private ObjectMapper mapper;
 
+
+    @MockBean
+    private MailSendService mailSendService;
 
 
     @Nested
-    @DisplayName("sendMail 테스트")
-    class sendMailTest{
+    @DisplayName("sendMailController 테스트")
+    class sendMailTest {
 
         @DisplayName("성공")
         @Test
         void whenSuccess() throws Exception {
 
-            Map<String,String> requestData = new HashMap<>();
-            requestData.put("email" ,"apple@naver.com");
-            requestData.put("title","title");
-            requestData.put("body","body");
+            Map<String, String> requestData = new HashMap<>();
+            requestData.put("email", "apple@naver.com");
+            requestData.put("title", "title");
+            requestData.put("body", "body");
             String body = mapper.writeValueAsString(
                     requestData
             );
+
+            given(mailSendService.send(any())).willReturn(MailSendResponse.success());
             //then
-            mvc.perform(post( "/mail/send")
+            mvc.perform(post("/mail/send")
                             .content(body) //HTTP Body에 데이터를 담는다
                             .contentType(MediaType.APPLICATION_JSON) //보내는 데이터의 타입을 명시
                     )
                     .andExpect(status().isOk());
-                    //.andExpect(content().string("1"));
+            //.andExpect(content().string("1"));
         }
 
 
         @DisplayName("실패, 이메일 정규식에 일치하지 않음")
         @Test
         void whenFailByEmailNotValid() throws Exception {
-            Map<String,String> requestData = new HashMap<>();
-            requestData.put("email","error");
-            requestData.put("title","title");
-            requestData.put("body","body");
+            Map<String, String> requestData = new HashMap<>();
+            requestData.put("email", "error");
+            requestData.put("title", "title");
+            requestData.put("body", "body");
             String body = mapper.writeValueAsString(
                     requestData
             );
+            given(mailSendService.send(any())).willReturn(MailSendResponse.success());
             //then
-            mvc.perform(post( "/mail/send")
+            mvc.perform(post("/mail/send")
                             .content(body) //HTTP Body에 데이터를 담는다
                             .contentType(MediaType.APPLICATION_JSON) //보내는 데이터의 타입을 명시
                     )
@@ -70,14 +87,15 @@ class MailSendControllerTest {
         @DisplayName("실패, title이 null임")
         @Test
         void whenFailTitleIsNULL() throws Exception {
-            Map<String,String> requestData = new HashMap<>();
-            requestData.put("email","apple@naver.com");
-            requestData.put("body","body");
+            Map<String, String> requestData = new HashMap<>();
+            requestData.put("email", "apple@naver.com");
+            requestData.put("body", "body");
             String body = mapper.writeValueAsString(
                     requestData
             );
+            given(mailSendService.send(any())).willReturn(MailSendResponse.success());
             //then
-            mvc.perform(post( "/mail/send")
+            mvc.perform(post("/mail/send")
                             .content(body) //HTTP Body에 데이터를 담는다
                             .contentType(MediaType.APPLICATION_JSON) //보내는 데이터의 타입을 명시
                     )
@@ -87,14 +105,15 @@ class MailSendControllerTest {
         @DisplayName("실패, body가 null임")
         @Test
         void whenFailBodyIsNULL() throws Exception {
-            Map<String,String> requestData = new HashMap<>();
-            requestData.put("email","apple@naver.com");
-            requestData.put("title","title");
+            Map<String, String> requestData = new HashMap<>();
+            requestData.put("email", "apple@naver.com");
+            requestData.put("title", "title");
             String body = mapper.writeValueAsString(
                     requestData
             );
+            given(mailSendService.send(any())).willReturn(MailSendResponse.success());
             //then
-            mvc.perform(post( "/mail/send")
+            mvc.perform(post("/mail/send")
                             .content(body) //HTTP Body에 데이터를 담는다
                             .contentType(MediaType.APPLICATION_JSON) //보내는 데이터의 타입을 명시
                     )
