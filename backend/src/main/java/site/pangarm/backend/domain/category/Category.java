b9e65a511f5a68d1859ff98434269b73d4d97ad5
jiwon83 <gkswldnj0803@gmail.com>
2024-03-19 15:23:32 +0900

    Ref: BE_카테고리_카테고리 엔티티 리팩토링
    S10P22A509-154
    
    기본 생성자 제약조건 설정, 카테고리명 unique 제약 조건 설정

--- backend/src/main/java/site/pangarm/backend/domain/category/Category.java
+++ backend/src/main/java/site/pangarm/backend/domain/category/Category.java
@@ -1,13 +1,28 @@
 package site.pangarm.backend.domain.category;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import site.pangarm.backend.application.category.dto.request.CategoryUpdateRequest;
+
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
-    
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id; //뉴스카테고리 아이디
 
-    @Column(nullable = false)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; //뉴스 카테고리 아이디
+
+    @Column(nullable = false, unique = true)
     private String name; //카테고리 이름
+
+    @Builder
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public void update(CategoryUpdateRequest request) {
+        this.name = request.name();
+    }
 }
