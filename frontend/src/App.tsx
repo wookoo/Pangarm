edf31a5a576e08476a7d5936847ebae21d4649ab
gwanu-dev <devkwanwoo@gmail.com>
2024-03-20 21:02:15 +0900

    Feat: FE_판례 리스트 basic UI 추가#1
    S10P22A509-140
    
    추가 정렬 필요

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./layouts/Header/Header";
+import PrecedentSearch from "./pages/PrecedentSearchPage";
+
 export default function App() {
   return (
     <>
-      App
+      <Header></Header>
+      <PrecedentSearch></PrecedentSearch>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,9 @@ import "./index.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const enableMocking = async () => {
   if (!import.meta.env.DEV) {
@@ -19,9 +22,11 @@ const enableMocking = async () => {
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <Theme>
-        <App />
-      </Theme>
-    </React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Theme>
+          <App />
+        </Theme>
+      </QueryClientProvider>
+    </React.StrictMode>,
   );
 });
