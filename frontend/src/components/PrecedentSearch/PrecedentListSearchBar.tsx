52a1d391839710d7976263c3ed32691d2d64671e
gwanu-dev <devkwanwoo@gmail.com>
2024-03-20 16:12:55 +0900

    Feat : FE_SearchBar UI 추가
    S10P22509-140

---
+++ frontend/src/components/PrecedentSearch/PrecedentListSearchBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface PrecedentListSearchBarProps {
+  content: string;
+}
+
+// export default function PrecedentListSearchBar({ content }: PrecedentListSearchBarProps) {
+export default function PrecedentListSearchBar() {
+  return (
+    <input className="mt-4 px-4 text-2xl flex h-16 w-11/12 items-center rounded-md border border-lightgray bg-lightblue text-navy"
+    placeholder="검색어를 입력해주세요..."/>
+  );
+}
