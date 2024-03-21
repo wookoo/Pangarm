236087c97b3539a850e3cdf3fb858fb21dab3a37
jiwon83 <gkswldnj0803@gmail.com>
2024-03-21 15:02:49 +0900

    Feat: BE_회원 카테고리 엔티티 변경
    S10P22A509-157

--- backend/src/main/java/site/pangarm/backend/domain/membercategory/MemberCategory.java
+++ backend/src/main/java/site/pangarm/backend/domain/membercategory/MemberCategory.java
@@ -1,28 +1,41 @@
 package site.pangarm.backend.domain.membercategory;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.*;
 import site.pangarm.backend.domain.category.Category;
 import site.pangarm.backend.domain.member.Member;
 
-@Builder
-@AllArgsConstructor @NoArgsConstructor
+@Getter
 @Entity
-@IdClass(MemberCategoryId.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberCategory {
-    
-    @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member memberId; //회원 아이디
-    
-    @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", nullable = false)
-    private Category categoryId; //카테고리 아이디
-    
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; //멤버 카테고리 아이디
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member; //회원 아이디
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category; //카테고리 아이디
+
+    @Builder
+    public MemberCategory(Member member, Category category) {
+        addRelatedMember(member);
+        addRelatedCategory(category);
+    }
+
+    private void addRelatedMember(Member member){
+        this.member = member;
+        member.getMemberCategoryList().add(this);
+    }
+
+    private void addRelatedCategory(Category category){
+        this.category = category;
+        category.getMemberCategoryList().add(this);
+    }
 
 }
