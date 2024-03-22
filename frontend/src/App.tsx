aa4b4e987659eee632584abddbe17b8fdd669481
김민준 <alswns1285@gmail.com>
2024-03-22 13:11:34 +0900

    Ref: FE_alias로 전환

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "./layouts/Header/Header";
+import Header from "@/layouts/Header/Header";
 
 export default function App() {
   return (
