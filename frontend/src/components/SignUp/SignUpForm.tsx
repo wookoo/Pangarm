dfb9a0b1bdbce24252ab8bfc38c827efcfc0b66e
김민준 <alswns1285@gmail.com>
2024-03-25 15:35:04 +0900

    Ref: FE_회원가입 폼 리팩토링
    S10P22A509-192
    
    - 이름 validation
    - 비밀번호 validation
    - 기존 axios에서 tanstack query로 관리

--- frontend/src/components/SignUp/SignUpForm.tsx
+++ frontend/src/components/SignUp/SignUpForm.tsx
@@ -1,15 +1,49 @@
-import SignUpFormFooter from "./SignUpFormFooter";
-import SignUpFormHeader from "./SignUpFormHeader";
-import SignUpFormMain from "./SignUpFormMain";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+
+import { SignUpFormInput } from "@/types";
+import { signUp } from "@/services/authService";
+
+import SignUpFormFooter from "@/components/SignUp/SignUpFormFooter";
+import SignUpFormHeader from "@/components/SignUp/SignUpFormHeader";
+import SignUpFormMain from "@/components/SignUp/SignUpFormMain";
 
 export default function SignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormInput>();
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation({
+    mutationFn: signUp,
+    onSuccess: (response) => {
+      const { message } = response.data;
+      const status = response.status;
+
+      if (status === 200) {
+        alert(message);
+        navigate("/");
+      }
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const onSubmit: SubmitHandler<SignUpFormInput> = (data: SignUpFormInput) => {
+    mutate(data);
+  };
+
   return (
     <form
-      action=""
-      className="w-1/2 p-20 pb-10 h-full border border-lightgray shadow-md"
+      className="h-full w-1/2 border border-lightgray p-20 pb-10 shadow-md"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <SignUpFormHeader />
-      <SignUpFormMain />
+      <SignUpFormMain register={register} errors={errors} />
       <SignUpFormFooter />
     </form>
   );
