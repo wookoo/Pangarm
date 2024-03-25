5c933c5d7e72bfe78edef6521bd6309e35643994
Gwanu <devkwanwoo@gmail.com>
2024-03-25 17:57:58 +0900

    Feat: FE_NewsPage 파일 생성
    S10P22A509-203

---
+++ frontend/src/pages/NewsPage.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+export default function NewsPage() {
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    
+  }, [])
+  return (
+    <div>NewsPage</div>
+  )
+}
