4d85c2bf8e932e6fa2d8c37fc6ac91b14d6ecbe8
김민준 <alswns1285@gmail.com>
2024-03-25 15:24:15 +0900

    Design: FE_디자인 수정
    S10P22A509-192
    
    - "이미 회원이신가요?" hover시 색깔 변화 animation 추가

--- frontend/src/components/SignUp/SignUpFormFooter.tsx
+++ frontend/src/components/SignUp/SignUpFormFooter.tsx
@@ -7,7 +7,7 @@ export default function SignUpFormFooter() {
         회원가입
       </button>
       <Link to={"/signin"}>
-        <div className="mt-2 w-full text-end text-lightgray">
+        <div className="mt-2 w-full text-end text-lightgray transition duration-300 ease-in-out hover:text-gray">
           이미 회원이신가요?
         </div>
       </Link>
