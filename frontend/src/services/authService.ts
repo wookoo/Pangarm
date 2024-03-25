efbbbb873e198ec0ea8b72a7c3ca73e6addc4150
김민준 <alswns1285@gmail.com>
2024-03-25 15:25:01 +0900

    Feat: FE_회원가입 요청 함수 분리
    S10P22A509-192

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,7 +1,12 @@
 import instance from "@/utils/http-commons";
-import { SignInFormInput } from "@/types";
+import { SignInFormInput, SignUpFormInput } from "@/types";
 
 export const signIn = async (signInData: SignInFormInput) => {
   const response = await instance.post("/member/sign-in", signInData);
   return response;
 };
+
+export const signUp = async (signUpData: SignUpFormInput) => {
+  const response = await instance.post("/member/sign-up", signUpData);
+  return response;
+}
