1b9c1a9d5425f0441d8f0dbcc6ea0bcc57c4ecc0
Gwanu <devkwanwoo@gmail.com>
2024-03-27 10:58:11 +0900

    Remove: FE_뉴스 페이지 삭제
    S10P22A509-203

--- frontend/src/pages/NewsPage.tsx
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from "react";
-
-export default function NewsPage() {
-  const [news, setNews] = useState();
-
-  useEffect(() => {}, []);
-  return <div>NewsPage</div>;
-}
