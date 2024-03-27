aaafaa3867af55bace0fa4137b6dc5e5ed443e7a
김민준 <alswns1285@gmail.com>
2024-03-27 17:07:42 +0900

    Feat: FE_최신 뉴스 UI 구현
    S10P22A509-197

---
+++ frontend/src/components/News/LatestNewsListItem.tsx
@@ -0,0 +1,20 @@
+export default function LatestNewsListItem() {
+  return (
+    <div className="h-44 w-[95%] p-2 shadow-md mb-4 mx-3">
+      <p className="truncate text-xl font-semibold">
+        뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목
+        뉴스 제목
+      </p>
+      <p className="mb-3 text-xs text-lightgray">2024.03.08. 오전 10:31</p>
+      <div className="flex h-24 flex-grow justify-between gap-2">
+        <div className="h-full w-1/2 bg-lightgray"></div>
+        <p className="h-full w-1/2 line-clamp-4">
+          뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용
+          뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용
+          뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용 뉴스 내용
+          뉴스 내용 뉴스 내용 뉴스 내용
+        </p>
+      </div>
+    </div>
+  );
+}
