3da05e10ccd3b6f4f5dbfa09f886b69428f01388
김민준 <alswns1285@gmail.com>
2024-03-31 00:37:44 +0900

    Feat: FE_api 요청 엔드포인트 수정
    S10P22A509-199

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -2,11 +2,11 @@ import instance from "@/utils/http-commons";
 import { SignInFormInput, SignUpFormInput } from "@/types";
 
 export const signIn = async (signInData: SignInFormInput) => {
-  const response = await instance.post("/member/sign-in", signInData);
+  const response = await instance.post("/member/signin", signInData);
   return response;
 };
 
 export const signUp = async (signUpData: SignUpFormInput) => {
-  const response = await instance.post("/member/sign-up", signUpData);
+  const response = await instance.post("/member/signup", signUpData);
   return response;
 }
--- frontend/src/utils/http-commons.ts
+++ frontend/src/utils/http-commons.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "",
+  baseURL: "/api",
   timeout: 1000,
 });
 
@@ -18,34 +18,34 @@ instance.interceptors.request.use(
   },
 );
 
-instance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// instance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    // 인증, 인가가 굳이 필요하지 않은 요청은 토큰 검사를 생략한다.
-    if(originalRequest.skipAuth) {
-      return Promise.reject(error);
-    }
+//     // 인증, 인가가 굳이 필요하지 않은 요청은 토큰 검사를 생략한다.
+//     if(originalRequest.skipAuth) {
+//       return Promise.reject(error);
+//     }
 
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      const refreshToken = localStorage.getItem("refreshToken");
-      try {
-        const { data } = await instance.post("/member/reissue", {
-          refreshToken,
-        });
-        localStorage.setItem("accessToken", data.accessToken);
-        instance.defaults.headers.common["Authorization"] =
-          `Bearer ${data.accessToken}`;
-        return instance(originalRequest);
-      } catch (refreshError) {
-        console.error("토큰 재발급 실패: ", refreshError);
-      }
-    }
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       const refreshToken = localStorage.getItem("refreshToken");
+//       try {
+//         const { data } = await instance.post("/member/reissue", {
+//           refreshToken,
+//         });
+//         localStorage.setItem("accessToken", data.accessToken);
+//         instance.defaults.headers.common["Authorization"] =
+//           `Bearer ${data.accessToken}`;
+//         return instance(originalRequest);
+//       } catch (refreshError) {
+//         console.error("토큰 재발급 실패: ", refreshError);
+//       }
+//     }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
 
 export default instance;
