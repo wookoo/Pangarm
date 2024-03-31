7a81804689571182c281a4a31113afeb1a4b2ec0
김도현 <nanayagoon@daum.net>
2024-03-31 22:39:39 +0900

    Fix: DATA_판례 API 다운로드 목록 마운트 안되는 현상 수정
    S10P22A509-217
    
    폴더 마운트가 안되서 삭제되는 상황 수정

--- data/crawler/law/precedent/api/precedent-list/run.sh
+++ data/crawler/law/precedent/api/precedent-list/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir data
+mkdir data/log
 docker build -t precedent-api-list-python .
-docker run --rm  --env-file .env precedent-api-list-python
+docker run --rm  --env-file .env -v ./data/log:/app/data/log precedent-api-list-python
