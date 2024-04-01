573e9178d4dd6c76a459f7345cf4339f93801917
김민준 <alswns1285@gmail.com>
2024-04-01 23:51:45 +0900

    Feat: FE_회원 메인 페이지 구현

--- frontend/src/components/Main/MainMember.tsx
+++ frontend/src/components/Main/MainMember.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import Logo from "@/assets/imgs/Logo.svg?react";
@@ -6,7 +7,7 @@ import MainSearchForm from "@/components/Main/MainSearchForm";
 import HistoryList from "@/components/Main/HistoryList";
 
 type Input = {
-  content: string;
+  situation: string;
 };
 
 export default function MainMember() {
@@ -14,9 +15,17 @@ export default function MainMember() {
     register,
     handleSubmit,
     reset,
+    setValue,
     watch,
     formState: { isValid },
   } = useForm<Input>({ mode: "onChange" });
+
+  const [selectedSituation, setSelectedSituation] = useState("");
+
+  const handleSelectSituation = (situation: string) => {
+    setSelectedSituation(situation);
+    setValue("situation", situation);
+  };
 
   return (
     <div className="mx-[300px] flex flex-col items-center gap-6">
@@ -24,13 +33,15 @@ export default function MainMember() {
       <div className="font-TitleMedium text-3xl text-yellow">판가름</div>
 
       <div className="flex gap-2">
-        <HistoryList />
+        <HistoryList onSelectSituation={handleSelectSituation} />
         <MainSearchForm
           register={register}
           handleSubmit={handleSubmit}
           reset={reset}
           watch={watch}
+          setValue={() => setValue}
           isValid={isValid}
+          selectedSituation={selectedSituation}
         />
       </div>
     </div>
