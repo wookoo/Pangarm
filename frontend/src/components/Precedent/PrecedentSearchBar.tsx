4fac731b56080e4db4ed881952056323a349a267
김민준 <alswns1285@gmail.com>
2024-04-01 15:44:28 +0900

    Feat: FE_상황 검색 데이터 동기화
    S10P22A509-194
    
    - 메인에서 검색하는 상황과 판례 검색 페이지에서 input에 있는 value 동기화

--- frontend/src/components/Precedent/PrecedentSearchBar.tsx
+++ frontend/src/components/Precedent/PrecedentSearchBar.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 
+import { useContentStore } from "@/stores/contentStore";
+
 export default function PrecedentSearchBar() {
+  const content = useContentStore((state) => state.content);
+
   const [isFocused, setIsFocused] = useState(false);
-  const [text, setText] = useState(""); // textarea의 내용을 관리하는 state
+  const [text, setText] = useState(content); // textarea의 내용을 관리하는 state
 
   return (
     <form
@@ -11,7 +15,7 @@ export default function PrecedentSearchBar() {
       // Form의 높이를 조절할 클래스를 조건부로 적용
     >
       <textarea
-        className="flex-1 resize-none items-center bg-transparent text-navy transition-all duration-300 ease-in-out"
+        className="flex-1 resize-none items-center bg-transparent text-navy outline-none transition-all duration-300 ease-in-out"
         style={{
           margin: "1rem", // 직접 마진 적용
           height: isFocused && text ? "12rem" : "4rem", // 직접 높이 조절
@@ -21,6 +25,7 @@ export default function PrecedentSearchBar() {
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         onChange={(e) => setText(e.target.value)}
+        value={text}
       />
       <IoSearchOutline />
     </form>
