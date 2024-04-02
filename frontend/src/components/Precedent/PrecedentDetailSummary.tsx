327d869d9eaa61f50e116c117cea185f9a4b924e
gwanu-dev <devkwanwoo@gmail.com>
2024-04-02 14:03:44 +0900

    Feat: FE_판례 검색 및 판례 페이지 자료 구조 일부 변경
    S10P22A509-251

--- frontend/src/components/Precedent/PrecedentDetailSummary.tsx
+++ frontend/src/components/Precedent/PrecedentDetailSummary.tsx
@@ -1,67 +1,7 @@
-import PrecedentDetailSummaryElement from "@/components/Precedent/PrecedentDetailSummaryElement";
-
-// type PrecedentDetailSummaryProps = {
-//   1: {
-//     1: string;
-//     2: string;
-//   };
-//   2: string;
-//   3: string;
-//   4: string;
-//   5: string;
-//   6: string;
-// }
-
-// export default function PrecedentDetailSummary({ summary } : PrecedentDetailSummaryProps) {
-
-const summary = {
-  1: {
-    1: "우주 제국력 9018년 12월 12일 황제 즉위 기념일에 그랬어요",
-    2: "A가 B에게 총을 발사했어요",
-  },
-  2: "신원 미상의 남성 A",
-  3: "우주 제국 경찰 ",
-  4: "A 씨는 우주 제국 경찰의 연설이 정치적으로 알맞지 않다고 판단했어요",
-  5: "B씨를 향해 총을 5발 발사했어요",
-  6: "B씨는 중상을 입었다.",
-};
-
 export default function PrecedentDetailSummary() {
   return (
     <div className=" m-10 flex h-[70vh] overflow-y-auto font-TitleLight text-lg ">
-      <div>
-        <p className="mt-5">1. 어떤 상황에 대한 것인가요?</p>
-        <div className="px-6 text-[15px]">
-          <PrecedentDetailSummaryElement
-            question="a. 언제 그랬나요?"
-            content={summary[1][1]}
-          />
-          <PrecedentDetailSummaryElement
-            question="b. 어떤 상황이었나요?"
-            content={summary[1][2]}
-          />
-        </div>
-        <PrecedentDetailSummaryElement
-          question="2. 원고는 누구인가요?"
-          content={summary[2]}
-        />
-        <PrecedentDetailSummaryElement
-          question="3. 피고는 누구인가요?"
-          content={summary[3]}
-        />
-        <PrecedentDetailSummaryElement
-          question="4. 왜 그랬나요?"
-          content={summary[4]}
-        />
-        <PrecedentDetailSummaryElement
-          question="5. 어떤 행동을 취했나요?"
-          content={summary[5]}
-        />
-        <PrecedentDetailSummaryElement
-          question="6. 어떤 결과가 있었나요?"
-          content={summary[6]}
-        />
-      </div>
+      <div></div>
     </div>
   );
 }
--- frontend/src/components/Precedent/PrecedentSearchCondition.tsx
+++ frontend/src/components/Precedent/PrecedentSearchCondition.tsx
@@ -7,25 +7,8 @@ import { useSearch } from "@/components/Precedent/SearchContext";
 import PrecedentSearchToggleSlider from "@/components/Precedent/PrecedentSearchToggleSlider";
 import PrecedentSearchSimilaritySlider from "@/components/Precedent/PrecedentSearchSimilaritySlider";
 
-// import { FiltersType } from "../../types";
-
-// function isKeyOfFiltersType(key: string, obj: FiltersType): key is keyof FiltersType {
-//   return key in obj;
-// }
-
 export default function PrecedentSearchCondition() {
   const { filters, setFilters } = useSearch();
-
-  // const handleClick = () => {
-  //   setFilters({
-  //     keywords: [],
-  //     startDate: "",
-  //     endDate: "",
-  //     isViewed: false,
-  //     isBookmarked: false,
-  //     minSimilarity: 50,
-  //   });
-  // };
 
   const setDate = (date: string, value: string) => {
     setFilters((t) => {
@@ -38,13 +21,13 @@ export default function PrecedentSearchCondition() {
 
   const setSimilarity = (value: number[]) => {
     setFilters((t) => {
-      t["minSimilarity"] = value[0];
+      t["minimumSimilarity"] = value[0];
       return { ...t };
     });
   };
 
   return (
-    <div className="sticky h-[500px] w-[300px] top-10">
+    <div className="sticky top-10 h-[500px] w-[300px]">
       <div className="flex items-center justify-between p-1">
         <p className="font-SubTitle text-xl">필터</p>
         <GrRefresh />
@@ -70,13 +53,13 @@ export default function PrecedentSearchCondition() {
       <div className="flex items-center justify-between px-1">
         <PrecedentSearchDateRangePicker
           setDate={setDate}
-          date={filters.startDate}
+          date={filters.duration.startDate}
           label="startDate"
         />{" "}
         ~{" "}
         <PrecedentSearchDateRangePicker
           setDate={setDate}
-          date={filters.endDate}
+          date={filters.duration.endDate}
           label="endDate"
         />
       </div>
@@ -93,7 +76,7 @@ export default function PrecedentSearchCondition() {
 
       <div className="flex items-center font-Content text-lg">
         최소 유사도 &nbsp;
-        <p className="font-yellow">{filters.minSimilarity} </p> % 이상
+        <p className="font-yellow">{filters.minimumSimilarity} </p> % 이상
       </div>
       <div className="mt-1 px-1 ">
         <PrecedentSearchSimilaritySlider setSimilarity={setSimilarity} />
