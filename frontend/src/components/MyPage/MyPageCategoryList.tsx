bb5162d615dde497ed6a17822b4faacdbe6854a4
gwanu-dev <devkwanwoo@gmail.com>
2024-04-02 13:58:31 +0900

    Feat: FE_마이페이지 카테고리 별 뉴스 보기
    S10P22A509-251

--- frontend/src/components/MyPage/MyPageCategoryList.tsx
+++ frontend/src/components/MyPage/MyPageCategoryList.tsx
@@ -1,18 +1,26 @@
 import ErrorEmptyAnimation from "../Error/ErrorEmptyAnimation";
 import Error500Animation from "../Error/Error500Animation";
 import MyPageCategory from "./MyPageCategory";
-import { postUnsubscribeCategory } from "@/services/newsService";
-import { useMutation } from "@tanstack/react-query";
+import { postUnsubscribeCategory } from "@/services/authService";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getCategoryList } from "@/services/newsService";
 
 type MyPageCategoryListProps = {
   categoryList: string[];
   handleCategoryClick: (category: string) => void;
+  currentCategory: string;
 };
 
 export default function MyPageCategoryList({
   categoryList,
   handleCategoryClick,
+  currentCategory,
 }: MyPageCategoryListProps) {
+  const { data } = useQuery({
+    queryKey: ["categoryList"],
+    queryFn: getCategoryList,
+  });
+
   const { mutate } = useMutation({
     mutationFn: postUnsubscribeCategory,
     onError: (error) => {
@@ -25,19 +33,21 @@ export default function MyPageCategoryList({
     },
   });
 
-  const handleUnsubscibeClick = (category: string) => {
-    mutate(category);
+  const handleUnsubscribeClick = (category: string) => {
+    const index = data?.data.data.indexOf(category);
+    mutate(index);
   };
 
   return (
-    <div className="shadow-inner-md m-2 w-3/12 flex-row justify-center rounded-lg border border-lightgray bg-lightblue shadow-black">
+    <div className="shadow-inner-md m-2 w-3/12 flex-row justify-center overflow-y-scroll rounded-lg border border-lightgray bg-lightblue shadow-black">
       {categoryList ? (
-        categoryList.length === 0 ? (
+        categoryList.length !== 0 ? (
           categoryList.map((value) => (
             <MyPageCategory
               handleCategoryClick={handleCategoryClick}
-              handleUnsubscibeClick={handleUnsubscibeClick}
+              handleUnsubscribeClick={handleUnsubscribeClick}
               category={value}
+              currentCategory={currentCategory}
               key={value}
             />
           ))
