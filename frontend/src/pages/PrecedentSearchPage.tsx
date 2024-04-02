97f785054ce3803a2dfe322a105afb4aa86f8548
gwanu-dev <devkwanwoo@gmail.com>
2024-04-03 03:00:22 +0900

    Feat: FE_검색 바 변수 변경 및 판례 리스트, 아이템 조정

--- frontend/src/pages/PrecedentSearchPage.tsx
+++ frontend/src/pages/PrecedentSearchPage.tsx
@@ -4,37 +4,80 @@ import PrecedentSearchCondition from "@/components/Precedent/PrecedentSearchCond
 import PrecedentList from "@/components/Precedent/PrecedentList";
 import { SearchProvider } from "@/components/Precedent/SearchContext";
 import PrecedentDetail from "@/components/Precedent/PrecedentDetail";
+import { postPrecedentSearch } from "@/services/precedentService";
+import { useMutation } from "@tanstack/react-query";
+import { PrecedentItemType } from "@/types";
 
 export default function PrecedentSearchPage() {
   const [detailVisible, setDetailVisible] = useState<boolean>(false);
   const [detailCaseNo, setDetailCaseNo] = useState<string>("");
-  const showDetail = (caseNo: string) => {
+
+  const [precedentList, setPrecedentList] = useState<PrecedentItemType[]>([]);
+  const [precedentItem, setPrecedentItem] = useState<PrecedentItemType>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [resultCount, setResultCount] = useState<number>(0);
+  const [keywordList, setKeywordList] = useState<string[]>([]);
+  const showDetail = (precedentData: PrecedentItemType) => {
     setDetailVisible(true);
-    setDetailCaseNo(caseNo);
+    setDetailCaseNo(precedentData.caseNumber);
+    setPrecedentItem(precedentData);
   };
 
   const closeDetail = () => {
     setDetailVisible(false);
   };
 
+  const handleLoading = () => {
+    setIsLoading(true);
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: postPrecedentSearch,
+    onSuccess: (res) => {
+      setIsLoading(false);
+      setIsError(false);
+      setResultCount(res.data.data.precedentList.totalElements);
+      setKeywordList(res.data.data.keywordList);
+      console.log(res);
+      setPrecedentList(res.data.data.precedentList.content);
+    },
+    onError: (err) => {
+      setIsLoading(false);
+      setIsError(true);
+      console.log(err);
+    },
+  });
+
   return (
     <SearchProvider>
       <div>
-        {detailVisible && (
-          <PrecedentDetail closeDetail={closeDetail} detailNo={detailCaseNo} />
+        {detailVisible && precedentItem && (
+          <PrecedentDetail
+            closeDetail={closeDetail}
+            caseNo={detailCaseNo}
+            precedentData={precedentItem}
+          />
         )}
         <div
           className={`mx-3 mt-3 w-full flex-row items-center justify-center gap-6 px-72 ${detailVisible ? " overflow-hidden " : ""}`}
         >
           <div className="mx-5 flex-row">
             <p className="font-SubTitle text-3xl">다시 검색하기</p>
-            <PrecedentSearchBar></PrecedentSearchBar>
+            <PrecedentSearchBar mutate={mutate} handleLoading={handleLoading} />
           </div>
           <div className="mx-5 mt-12 flex">
-            <PrecedentList showDetail={showDetail} />
+            <PrecedentList
+              showDetail={showDetail}
+              precedentList={precedentList}
+              isLoading={isLoading}
+              isError={isError}
+              mutate={mutate}
+              resultCount={resultCount}
+            />
             <div className=" mx-7 w-[1px] bg-lightgray" />
             <div>
-              <PrecedentSearchCondition></PrecedentSearchCondition>
+              <PrecedentSearchCondition keywordList={keywordList} />
             </div>
           </div>
         </div>
