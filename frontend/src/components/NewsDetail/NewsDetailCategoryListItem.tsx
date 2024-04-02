5f7ecd6a7b6d5084915ac143fada6cfcd52b18c6
김민준 <alswns1285@gmail.com>
2024-04-03 01:18:55 +0900

    Fix: FE_뉴스 카테고리 구독 기능 수정
    S10P22A509-272

--- frontend/src/components/NewsDetail/NewsDetailCategoryListItem.tsx
+++ frontend/src/components/NewsDetail/NewsDetailCategoryListItem.tsx
@@ -1,7 +1,9 @@
+import Swal from "sweetalert2";
 import axios, { HttpStatusCode } from "axios";
 import { useMutation } from "@tanstack/react-query";
 
 import { postSubscribeNewsCategory } from "@/services/authService";
+import { useAuthStore } from "@/stores/authStore";
 
 export default function NewsDetailCategoryListItem({
   category,
@@ -11,22 +13,38 @@ export default function NewsDetailCategoryListItem({
   const { mutate } = useMutation({
     mutationFn: (category: string) => postSubscribeNewsCategory(category),
   });
+  const isSignedIn = useAuthStore((state) => state.isSignedIn);
 
   const handleSubscribe = () => {
+    if (!isSignedIn) {
+      Swal.fire({
+        title: "로그인이 필요한 서비스입니다!",
+        icon: "error",
+      });
+      return;
+    }
+
     mutate(category, {
       onSuccess: (response) => {
         const status = response.status;
 
         if (status === HttpStatusCode.Created) {
-          alert(`'${category}' 카테고리 구독에 성공했습니다!`);
+          Swal.fire({
+            text: `'${category}' 카테고리 구독에 성공했습니다!`,
+            icon: "success",
+          });
           return;
         }
       },
       onError: (error) => {
         if (axios.isAxiosError(error)) {
           const response = error.response;
+
           if (response?.status === HttpStatusCode.Conflict) {
-            alert("이미 구독한 카테고리입니다");
+            Swal.fire({
+              text: "이미 구독한 카테고리입니다!",
+              icon: "warning",
+            });
             return;
           }
         } else {
