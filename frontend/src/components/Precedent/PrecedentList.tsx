4fab2a52e4075b3309c1b261222267eaac036557
gwanu-dev <devkwanwoo@gmail.com>
2024-04-03 17:07:59 +0900

    Design: FE_검색 기록 UI 변경
    S10P22A509-255

--- frontend/src/components/Precedent/PrecedentList.tsx
+++ frontend/src/components/Precedent/PrecedentList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { AxiosResponse } from "axios";
 
 import PrecedentListItem from "@/components/Precedent/PrecedentListItem";
@@ -11,6 +11,8 @@ import PrecedentLoadingAnimation from "@/components/PrecedentLoadingAnimation";
 import Error500Animation from "@/components/Error/Error500Animation";
 import { PrecedentItemType } from "@/types";
 import { useSituationStore } from "@/stores/situationStore";
+import ErrorEmptyAnimation from "../Error/ErrorEmptyAnimation";
+import { useInView } from "react-intersection-observer";
 
 interface PrecedentListProps {
   precedentList: PrecedentItemType[];
@@ -35,15 +37,39 @@ export default function PrecedentList({
 }: PrecedentListProps) {
   const { filters } = useSearch();
   const situation = useSituationStore((state) => state.situation);
+  const size = useRef<number>(10);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    mutate({ situation: situation, page: 0, size: 100, filter: filters });
+    if (situation !== "")
+      mutate({
+        situation: situation,
+        page: 0,
+        size: size.current,
+        filter: filters,
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (inView) {
+      size.current = size.current + 10;
+      mutate({
+        situation: situation,
+        page: 0,
+        size: size.current,
+        filter: filters,
+      });
+    }
+  }, [filters, inView, mutate, situation]);
+
   return (
     <div className="w-8/12 flex-row">
-      {isLoading ? (
+      {situation === "" ? (
+        <div className="flex h-full flex-col items-center justify-center">
+          <p className="font-TitleLight text-3xl">검색어를 입력해주세요</p>
+        </div>
+      ) : isLoading ? (
         <div>
           <PrecedentLoadingAnimation />
         </div>
@@ -54,6 +80,8 @@ export default function PrecedentList({
             무언가 잘못됐어요... 나중에 다시 시도해주세요
           </p>
         </div>
+      ) : resultCount === 0 ? (
+        <ErrorEmptyAnimation />
       ) : (
         <>
           <div className="mb-3 flex justify-between">
@@ -71,13 +99,21 @@ export default function PrecedentList({
           </div>
           <div>
             {precedentList &&
-              precedentList.map((precedentData: PrecedentItemType) => (
-                <React.Fragment key={precedentData.caseNumber}>
-                  <PrecedentListItem precedentData={precedentData} />
-                  <hr className="opacity-30" />
-                </React.Fragment>
-              ))}
+              precedentList.map((precedentData: PrecedentItemType) => {
+                precedentData.similarity = Math.min(
+                  precedentData.similarity,
+                  100,
+                );
+                return (
+                  <React.Fragment key={precedentData.caseNumber}>
+                    <PrecedentListItem precedentData={precedentData} />
+                    <hr className="opacity-30" />
+                  </React.Fragment>
+                );
+              })}
           </div>
+
+          <div ref={ref} className="h-2 w-full" />
         </>
       )}
     </div>
