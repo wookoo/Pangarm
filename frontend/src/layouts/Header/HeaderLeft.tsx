9a9334d1b58df185d3ee72fa14ebf092a4b58c03
gwanu-dev <devkwanwoo@gmail.com>
2024-04-03 10:36:39 +0900

    Feat: FE_판례 찾기 페이지 tsx 및 인라인 HTML css 추가
    S10P22A509-255

--- frontend/src/layouts/Header/HeaderLeft.tsx
+++ frontend/src/layouts/Header/HeaderLeft.tsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function HeaderLeft() {
   return (
     <Flex gap="9">
-      <Box>판례 검색</Box>
+      <Link to="precedent-search">
+        <Box>판례 검색</Box>
+      </Link>
       <Link to="news">
         <Box>법 뉴스</Box>
       </Link>
