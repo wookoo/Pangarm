9e1ebad070ae8f45cefc1ff694e5e06b4b32373f
gwanu-dev <devkwanwoo@gmail.com>
2024-04-03 17:04:33 +0900

    Feat: FE_Footer 추가
    S10P22A509-255

--- frontend/src/components/MyPage/MyPagePrecedent.tsx
+++ frontend/src/components/MyPage/MyPagePrecedent.tsx
@@ -37,7 +37,7 @@ export default function MyPagePrecedent({
   const precedentData = data?.data.data.precedentList;
   console.log(precedentData);
   return (
-    <div className="flex max-h-[60vh] flex-wrap overflow-y-scroll">
+    <div className="flex h-[60vh] flex-wrap overflow-y-scroll">
       {precedentData ? (
         precedentData.length !== 0 ? (
           precedentData.map(
@@ -64,10 +64,14 @@ export default function MyPagePrecedent({
             ),
           )
         ) : (
-          <ErrorEmptyAnimation />
+          <div className="flex h-full">
+            <ErrorEmptyAnimation />
+          </div>
         )
       ) : (
-        <Error500Animation />
+        <div className="flex h-full">
+          <Error500Animation />
+        </div>
       )}
     </div>
   );
