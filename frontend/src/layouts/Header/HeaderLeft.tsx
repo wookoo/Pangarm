1392622de54eef010291d4e80b8a4895cac26f65
김민준 <alswns1285@gmail.com>
2024-04-03 10:54:24 +0900

    Feat: FE_판례 검색 라우팅 추가

--- frontend/src/layouts/Header/HeaderLeft.tsx
+++ frontend/src/layouts/Header/HeaderLeft.tsx
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 export default function HeaderLeft() {
   return (
     <Flex gap="9">
-      <Box>판례 검색</Box>
+      <Link to="precedent-search">
+        <Box>판례 검색</Box>
+      </Link>
       <Link to="news">
         <Box>법 뉴스</Box>
       </Link>
-      <Box>법령 사전</Box>
     </Flex>
   );
 }
